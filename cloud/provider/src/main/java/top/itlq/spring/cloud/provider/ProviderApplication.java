560bd3d68a4a022b9f6b634bb6bde3793fb0ca64
dlmu-lq <18018968632@163.com>
2021-01-17 15:54:06 +0800

    old

--- cloud/provider/src/main/java/top/itlq/spring/cloud/provider/ProviderApplication.java
+++ cloud/provider/src/main/java/top/itlq/spring/cloud/provider/ProviderApplication.java
@@ -1,5 +1,6 @@
 package top.itlq.spring.cloud.provider;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDubbo
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
