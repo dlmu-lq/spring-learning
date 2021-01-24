f34e082b7c97de85cab59c52396822e98cbfa2cc
dlmu-lq <18018968632@163.com>
2021-01-24 21:42:59 +0800

    nacos registry

--- cloud/cloud-provider/src/main/java/top/itlq/spring/cloud/provider/service/UserServiceImpl.java
+++ cloud/cloud-provider/src/main/java/top/itlq/spring/cloud/provider/service/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package top.itlq.spring.cloud.provider.service;
 
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 import top.itlq.spring.cloud.interfaces.entity.UserEntity;
 import top.itlq.spring.cloud.interfaces.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Value("${server.port}")
+    private Integer port;
+
     @Override
     public UserEntity getUser(Long id) {
-        return new UserEntity(1L, "test", "test");
+        return new UserEntity(1L, "test from port" + port, "test");
     }
 }
